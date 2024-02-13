1738d6c81861a1f09764c5debabf1a55d2db64eb
Andrei Raketa <raketa420311@gmail.com>
2024-02-13 22:51:52 +0300

    destroyable obstacles update

--- Assets/Scripts/DestroyableObjects/DestroyableObjectObstacle.cs
+++ Assets/Scripts/DestroyableObjects/DestroyableObjectObstacle.cs
@@ -14,6 +14,8 @@ public class DestroyableObjectObstacle : DestroyableObject
         if (!playerCollider)
             return;
         
+        Debug.Log($"OnCollidedPlayer");
+        
         OnCollidedPlayer?.Invoke();
     }
 }
--- Assets/Scripts/PlayerController/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerController/PlayerBehaviour.cs
@@ -4,7 +4,9 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private SimplyMover _movement;
     [SerializeField] private ProjectileLauncher _projectileLauncher;
+    [SerializeField] private PlayerCollider _playerCollider;
 
     public IMover Movement => _movement;
     public IProjectileLauncher ProjectileLauncher => _projectileLauncher;
+    public PlayerCollider PlayerCollider => _playerCollider;
 }
